f2d74b501393057c4265db01e5f6a1c99396a2f4
AlexLopez16 <aalejandro.lopezr@hotmail.com>
2023-03-07 17:53:32 -0600

    change https

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,9 +1,10 @@
 import http from 'http';
+import https from 'https'
 import { Server as SocketServer, } from "socket.io";
 import { getRoom } from './helpers/getRoom';
 const server = require('./server');
 
-const app = http.createServer(server)
+const app = https.createServer(server)
 
 const io = new SocketServer(app, {
     cors: {
